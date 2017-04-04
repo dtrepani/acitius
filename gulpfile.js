09cabedd96f90fd4c0bee26efa1f38f1622282c5
Fina <finapani@gmail.com>
2017-04-03 20:16:53 -0400

    Progress on layout.

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,30 @@ const gulpLoadPlugins = require('gulp-load-plugins');
 const $ = gulpLoadPlugins();
 
 const dirPaths = {
-	js: 'app/',
-	css: 'app/content/css/',
-	scss: 'src/content/scss/'
+	js: 'app',
+	css: 'app/content/css',
+	scss: 'src/content/scss'
 };
 
 const filePaths = {
 	scss: [`${dirPaths.scss}/app.scss`],
 	// Watch for changes in any of the scss partials.
-	scssWatch: [`${dirPaths.scss}**/*.scss`]
+	scssWatch: [`${dirPaths.scss}/**/*.scss`]
 };
 
+const onError = $.notify.onError('<%= error.message %>');
+
 gulp.task('css', () => {
-	gulp.src(filePaths.scss)
-		.pipe($.sass())
-		.pipe(gulp.dest(dirPaths.css))
-		.pipe($.rename({ extname: '.min.css' }))
-		.pipe($.cssnano())
-		.pipe($.sourcemaps.write('.'))
-		.pipe(gulp.dest(dirPaths.css));
+	setTimeout(() => {
+		gulp.src(filePaths.scss)
+			.pipe($.plumber({ errorHandler: onError }))
+			.pipe($.sass())
+			.pipe(gulp.dest(dirPaths.css))
+			.pipe($.rename({ extname: '.min.css' }))
+			.pipe($.cssnano())
+			.pipe($.sourcemaps.write('.'))
+			.pipe(gulp.dest(dirPaths.css));
+	}, 500);
 });
 
 gulp.task('watch', () => {
